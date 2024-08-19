50a1b642ea7ca71eccd46781209b61e2948d129a
unknown <jp.heidbrink@gmail.com>
2024-08-18 20:51:38 -0500

    modified table. No callbacks.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,27 @@
+import satData from "./satData";
+
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {satData.map((sat) => (
+          <tr key={sat.id}>
+            <td>{sat.name}</td>
+            <td>{sat.type}</td>
+            <td>{sat.launchDate}</td>
+            <td>{sat.orbitType}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
