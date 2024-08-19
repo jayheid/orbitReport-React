8a8a2aabe9e9ab54bf28cac4e288b380a4429d14
unknown <jp.heidbrink@gmail.com>
2024-08-18 19:35:52 -0500

    modified task 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
 import Banner from "./components/Banner";
+import useState from "react";
+import SatData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(sataData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
